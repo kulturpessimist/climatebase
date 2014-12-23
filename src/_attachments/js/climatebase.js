a6e6ff5ca3371f148c569f7b508936c7e13b6b11
Alex <con.sume.org@gmail.com>
2014-12-23 03:45:17 +0100

    late night commit

--- src/_attachments/js/climatebase.js
+++ src/_attachments/js/climatebase.js
@@ -1,51 +1,62 @@
 var climatebase = climatebase || {
 	directives: {
-		humanDate: function(params){
-			return new Date(this.date).toLocaleDateString();
-		}
+		humanDate: {
+			text: function(params){
+				return new Date(this.date).toLocaleDateString();
+			}
+		}	
 	},
 	seek: function(){
-		if($('#details')[0].classList.contains('results')){
+		if($('#details').hasClass('results')){
 			climatebase.reset();
 		}else{
 			if( $('#searchfield').val()!=="" ){
 				$.getJSON( "/api/search/"+$('#searchfield').val())
 					.done(function( data ) {
-				  		console.log( "Data Loaded: ", data );
+				  		//console.log( "Data Loaded: ", data );
 						if(data.rows.length>0){
-							console.log('found!', data.rows[0]);
-							$('#start-search span')[0].classList.remove('glyphicon-search');
-							$('#start-search span')[0].classList.add('glyphicon-refresh');
+							console.log('found ->', data.rows[0]);
+							$('#start-search span').removeClass('glyphicon-search');
+							$('#start-search span').addClass('glyphicon-refresh');
 							// we found the thing
 							climatebase.fadeInTable(data.rows[0].doc);
+						}else{
+							climatebase.nope();
 						}
 					});
+			}else{
+				climatebase.nope();
 			}
 		}
+		return false;
 	},
-
 	fadeInTable: function(){
 		var data = arguments[0] || {};
 		$('#details').render(data, climatebase.directives);
 		if(data.customer.hidden){
 			$('#customer').remove();
 		}
-
-		$('#details')[0].classList.toggle('results');
-		$('.search')[0].classList.toggle('action');
+		$('#details').toggleClass('results');
+		$('.search').toggleClass('action');
 		var job = setTimeout(function() {
-			$('#start-search span')[0].classList.remove('glyphicon-refresh');
-			$('#start-search span')[0].classList.add('glyphicon-remove');
+			$('#start-search span').removeClass('glyphicon-refresh');
+			$('#start-search span').addClass('glyphicon-remove');
 		}, 1000);
 	},
-
+	nope: function(){
+		$('form').addClass('shake animated')
+			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$(this).removeClass('shake animated');
+			});	
+	},
+	/**/	
 	reset: function(){
-		$('#details')[0].classList.remove('results');
-		$('.search')[0].classList.remove('action');
+		$('#details').removeClass('results');
+		$('.search').removeClass('action');
 		var job = setTimeout(function() {
-			$('#start-search span')[0].classList.remove('glyphicon-refresh');
-			$('#start-search span')[0].classList.remove('glyphicon-remove');
-			$('#start-search span')[0].classList.add('glyphicon-search');
+			$('#start-search span').removeClass('glyphicon-refresh');
+			$('#start-search span').removeClass('glyphicon-remove');
+			$('#start-search span').addClass('glyphicon-search');
 		}, 1000);
 	},
 
@@ -55,10 +66,10 @@ var climatebase = climatebase || {
 			return v.toString(16);
 		});
 	},
-
+	/**/
 	init: function(){
-		$('#start-search').click(climatebase.seek)
+		$('#start-search').click(climatebase.seek);
+		$('form').on('submit', climatebase.seek);
 	}
 };
-
 climatebase.init();
